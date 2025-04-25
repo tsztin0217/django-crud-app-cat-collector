a5012966955f3459d2432e138f1c652387dbbfd9
tsztin0217 <tsztin0217@gmail.com>
2025-04-25 07:52:20 -0400

    Add FeedingForm to cat_detail view and handle form submission in add_feeding

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,11 @@
 # main_app/views.py
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat
+from .forms import FeedingForm
+
+
 
 # Import HttpResponse to send text-based responses
 # from django.http import HttpResponse
@@ -54,7 +57,24 @@ def cat_index(request):
 
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
-    return render(request, 'cats/detail.html', {'cat': cat})
+    # instantiate FeedingForm to be rendered in the template
+    feeding_form = FeedingForm()
+    return render(request, 'cats/detail.html', {
+        # include the cat and feeding_form in the context
+        'cat': cat, 'feeding_form': feeding_form
+    })
+
+def add_feeding(request, cat_id):
+    # create a ModelForm instance using the data in request.POST
+    form = FeedingForm(request.POST)
+    # validate the form
+    if form.is_valid():
+        # don't save the form to the db until it
+        # has the cat_id assigned
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = cat_id
+        new_feeding.save()
+    return redirect('cat-detail', cat_id=cat_id)
 
 
 class CatCreate(CreateView):
