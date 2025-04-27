36d9cdc1bdb2c06a3cfc92e6c6856f434db4061f
tsztin0217 <tsztin0217@gmail.com>
2025-04-27 18:04:52 -0400

    Import toy

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Cat, Feeding
+from .models import Cat, Feeding, Toy
 # Register your models here.
-admin.site.register([Cat, Feeding])
+admin.site.register([Cat, Feeding, Toy])
