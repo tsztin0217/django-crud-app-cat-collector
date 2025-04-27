731f88b9d6c9976aa3c4817461c524532456d824
tsztin0217 <tsztin0217@gmail.com>
2025-04-27 18:04:39 -0400

    Add toy model

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # A tuple of 2-tuples added above our models
 MEALS = (
@@ -9,11 +10,24 @@ MEALS = (
 )
 
 
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('toy-detail', kwargs={'pk': self.id})
+
+
 class Cat(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+    toys = models.ManyToManyField(Toy)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -41,3 +55,4 @@ class Feeding(models.Model):
     # Define the default order of feedings
     class Meta:
         ordering = ['-date']  # This line makes the newest feedings appear first
+
